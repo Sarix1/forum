d819fe67ef34e67f1c4bf58bfff52845ccb59785
Sarix1 <sari.katariina.jokinen@protonmail.com>
2020-09-11 08:38:21 +0300

    Add MVC for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :content))
+
+    @post.save
+    redirect_to @post
+  end
+end
---
+++ db/migrate/20200909212915_create_boards.rb
@@ -0,0 +1,11 @@
+class CreateBoards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :boards do |t|
+      t.string :title
+      t.string :description
+      t.integer :order
+
+      t.timestamps
+    end
+  end
+end
